79c676c58ab6e8ce8d5826ab90ce9e1fd85c303f
bryangalindo <galindo.bryan08@gmail.com>
2022-03-09 16:22:24 -0600

    minor refactor to incr readability

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, jsonify
 import time
-from redis import (
-    ConnectionPool,
-    StrictRedis
-)
+
+from flask import Flask, render_template, jsonify
+from redis import ConnectionPool, StrictRedis
+
 
 app = Flask(__name__)
 
@@ -11,21 +10,30 @@ connection_pool = ConnectionPool().from_url(url="redis://localhost:6379")
 redis_client = StrictRedis(connection_pool=connection_pool)
 
 
-def initialize_scoreboard():
+def set_scoreboard():
     for i in range(1, 10):
-        redis_client.zadd(name="scoreboard:1", mapping={f'player:{i}': 0})
+        redis_client.zadd(name="scoreboard:1",
+                          mapping={f'player:{i}': 0})
+
+
+def get_scores():
+    z_scores = redis_client.zrange(name="scoreboard:1",
+                                   start=0,
+                                   end=-1,
+                                   desc=True,
+                                   withscores=True)
+    return [{'player_id': score[0].decode(), 'score': score[1]} for score in z_scores]
 
 
 @app.route('/')
 def home():
-    initialize_scoreboard()
+    set_scoreboard()
     return render_template('home.html')
 
 
 @app.route('/scores')
 def scores():
-    z_scores = redis_client.zrange(name="scoreboard:1", start=0, end=-1, desc=True, withscores=True)
-    z_scores = [{'player_id': score[0].decode(), 'score': score[1]} for score in z_scores]  # can't jsonify bytes
+    z_scores = get_scores()
     return jsonify(scores=z_scores, last_updated_at=time.time())
 
 
