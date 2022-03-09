db9f8e61201ace926972b62962875c7bd2279350
bryangalindo <galindo.bryan08@gmail.com>
2022-03-09 17:03:20 -0600

    minor refactor to incr readability

--- increase_score_task.py
+++ increase_score_task.py
@@ -1,6 +1,8 @@
-from redis import ConnectionPool, StrictRedis
 import random
 import time
+
+from redis import ConnectionPool, StrictRedis
+
 
 connection_pool = ConnectionPool().from_url(url="redis://localhost:6379")
 redis_client = StrictRedis(connection_pool=connection_pool)
@@ -10,5 +12,7 @@ if __name__ == '__main__':
     while True:
         for i in range(1, 10):
             score_amount = random.randint(-5, 15)
-            redis_client.zincrby("scoreboard:1", value=f'player:{i}', amount=score_amount)
+            redis_client.zincrby("scoreboard:1",
+                                 value=f'player:{i}',
+                                 amount=score_amount)
         time.sleep(0.5)
